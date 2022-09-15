d7ecf4f35b901d21fc7eb06956a7e80964a2a68e
mausi <manster161@gmail.com>
2022-09-15 09:49:19 +0200

    Clean up

--- midihub.js
+++ midihub.js
@@ -1,21 +1,13 @@
+const mapping = new Map();
 var midihub = {
-    mapping: mapp = new Map(),
-    
     addMapping: function (ccvalue, callback){
-        this.mapping.set(ccvalue, callback);
+        mapping.set(ccvalue, callback);
     },
-
     newMessage: function (message){
-        if (this.mapping.has(message[1])){
-            this.mapping.get(message[1])(message[2]);
+        if (mapping.has(message[1])){
+            mapping.get(message[1])(message[2]);
         };
         console.log(`cc:${message[1]} value:${message[2]}`);
-    },
-    set: function (ccarray){
-        this.arr = ccarray;
-    },
-    array: function(){
-        return this.arr;
-    },
+    }
 };
 module.exports = midihub;
